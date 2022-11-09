5ab10a6c9dce550e054de97451f3f3833e491521
hiliyongke <249191296@qq.com>
2022-11-09 10:58:59 +0000

    Deploying to gh-pages from @ hiliyongke/vue3-admin-boilerplate@a68219d2adde1d67136d11a95209e5e4513e8aa7 🚀

---
+++ static/js/list-cbaf6115.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index-8f8d23fb.js";const r="mock/api/get-list",e="mock/api/get-card-list";function i(){return t.get({url:r})}function n(){return t.get({url:e})}export{n as a,i as g};
+//# sourceMappingURL=list-cbaf6115.js.map
